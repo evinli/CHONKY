4c005fdbd8d9e8a3f8a9d74a74f2132232258755
Evin Li <evinywli@gmail.com>
2022-07-05 12:12:24 -0700

    Added starter files

---
+++ include/PID.h
@@ -0,0 +1,13 @@
+/**
+ * @file      PID.h
+ * @author    Creators of CHONKY 
+ * @brief     Header file for the PID feedback-control class
+ * 
+ * @copyright Copyright (c) 2022
+ */
+
+// include guard that prevents header files from being compiled multiple times
+#pragma once
+
+// include all header file dependencies here
+#include "pins.h"
---
+++ include/constants.h
@@ -0,0 +1,16 @@
+/**
+ * @file      constants.h
+ * @author    Creators of CHONKY 
+ * @brief     List of all constants used for CHONKY firmware
+ * 
+ * @copyright Copyright (c) 2022
+ */
+
+// include guard that prevents header files from being compiled multiple times
+#pragma once
+
+// include all header file dependencies here
+#include "pins.h"
+
+// constant definitions
+#define ANALOG_READ_RESOLUTION 12
