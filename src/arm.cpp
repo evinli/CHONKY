b9a38452fe6783bd4e88594be638119ed9341c66
adarshgovi <adarshg2001@gmail.com>
2022-08-01 00:01:50 -0700

    updated constants for arm

--- src/arm.cpp
+++ src/arm.cpp
@@ -13,9 +13,9 @@ Arm::Arm(Motor* shoulder, Servo* elbow, Servo* claw, Servo* base, int shoulderSp
     this->claw = claw;
     this->base = base;
     this->shoulderSpeed = shoulderSpeed;
-    this->verticalSonar=verticalSonar;
-    this->horizontalSonar=horizontalSonar;
-    this->display=display;
+    this->verticalSonar = verticalSonar;
+    this->horizontalSonar = horizontalSonar;
+    this->display = display;
 }
 
 /////////////////// METHODS ///////////////////
@@ -26,16 +26,16 @@ void Arm::moveInPlaneShoulderFirst(double distanceFromChassis, double heightAbov
     double alpha = getAlpha(heightAboveGround, distanceFromChassis);
     double shoulderJointAngle = alpha + theta;
     moveShoulderJoint(shoulderJointAngle);
-    elbow->slowWrite(180-phi,8);
+    elbow->slowWrite(180 - phi, 8);
 }
 
-void Arm::moveInPlaneElbowFirst(double distanceFromChassis, double heightAboveGround){
+void Arm::moveInPlaneElbowFirst(double distanceFromChassis, double heightAboveGround) {
     double hypotenuse = getHypotenuse(heightAboveGround, distanceFromChassis);
     double phi = getPhi(hypotenuse);
     double theta = getTheta(hypotenuse, phi);
     double alpha = getAlpha(heightAboveGround, distanceFromChassis);
     double shoulderJointAngle = alpha + theta;
-    elbow->slowWrite(180-phi,8);
+    elbow->slowWrite(180 - phi, 8);
     moveShoulderJoint(shoulderJointAngle);
 }
 
@@ -44,18 +44,19 @@ void Arm::grasp() {
 }
 
 void Arm::moveShoulderJoint(int angle) {
+    if(angle>90||angle<0){
+        return;
+    }
     int potValue = analogRead(SHOULDER_POT);
-    int inputValue = angle*(SHOULDER_NINETY - SHOULDER_ZERO)/(SHOULDER_RANGE) + SHOULDER_ZERO; // input angle in terms of the potentiometer values
+    int inputValue = angle * (SHOULDER_NINETY - SHOULDER_ZERO) / (SHOULDER_RANGE) + SHOULDER_ZERO;  // input angle in terms of the potentiometer values
 
-    while (abs(potValue - inputValue) > POT_MOTOR_ERROR) { 
+    while (abs(potValue - inputValue) > POT_MOTOR_ERROR) {
         if (potValue < inputValue) {
             shoulder->setSpeed(shoulderSpeed);
-            while (potValue < inputValue)
-            {
+            while (potValue < inputValue) {
                 potValue = analogRead(SHOULDER_POT);
             }
-        }
-        else if (potValue > inputValue) {
+        } else if (potValue > inputValue) {
             shoulder->setSpeed(-shoulderSpeed);
             while (potValue > inputValue) {
                 potValue = analogRead(SHOULDER_POT);
@@ -65,23 +66,22 @@ void Arm::moveShoulderJoint(int angle) {
     shoulder->stop();
 }
 
-void Arm::rotateBase(int angle){
+void Arm::rotateBase(int angle) {
     int potValue = analogRead(BASE_POT);
-    int targetValue = angle*(526-250)/(180-90) -26; // TO BE MADE TO CONSTANTS
+    int targetValue = angle * (526 - 250) / (180 - 90) - 26;  // TO BE MADE TO CONSTANTS
 
     display->clear();
-    display->write(0,std::to_string(targetValue));
-    display->write(20,std::to_string(angle));
-    
-    while (abs(potValue - targetValue) > POT_MOTOR_ERROR) { 
+    display->write(0, std::to_string(targetValue));
+    display->write(20, std::to_string(angle));
+
+    while (abs(potValue - targetValue) > POT_MOTOR_ERROR) {
         if (potValue < targetValue) {
-            base->write(90); // change into constants
+            base->write(90);  // change into constants
             while (potValue < targetValue) {
                 potValue = analogRead(BASE_POT);
             }
-        }
-        else if (potValue > targetValue) {
-            base->write(75); // change into constants
+        } else if (potValue > targetValue) {
+            base->write(75);  // change into constants
             while (potValue > targetValue) {
                 potValue = analogRead(BASE_POT);
             }
@@ -91,7 +91,7 @@ void Arm::rotateBase(int angle){
     base->write(BASE_SERVO_STOP_ANGLE);
 }
 
-void Arm::dropInBasket(){
+void Arm::dropInBasket() {
     moveShoulderJoint(90);
     this->elbow->write(30);
     rotateBase(180);
@@ -100,58 +100,56 @@ void Arm::dropInBasket(){
     delay(3000);
 }
 
-void Arm::sweep(double startingDist, double endingDist, double height){
-    for(double i=startingDist;i<endingDist;i+=SWEEP_STEP_SIZE){
-        moveInPlaneShoulderFirst(i,height);
+void Arm::sweep(double startingDist, double endingDist, double height) {
+    for (double i = startingDist; i < endingDist; i += SWEEP_STEP_SIZE) {
+        moveInPlaneShoulderFirst(i, height);
         delay(8);
     }
 }
 
-
-void Arm::sweepAndDetect(double startingDist, double endingDist, double height){
+void Arm::sweepAndDetect(double startingDist, double endingDist, double height) {
     this->claw->write(50);
-    for(double i=startingDist;i<endingDist;i+=SWEEP_STEP_SIZE){
-        moveInPlaneShoulderFirst(i,height);
-        if (avgSampleSonar(SWEEP_SAMPLE_COUNT,this->verticalSonar)<10){
+    for (double i = startingDist; i < endingDist; i += SWEEP_STEP_SIZE) {
+        moveInPlaneShoulderFirst(i, height);
+        if (avgSampleSonar(SWEEP_SAMPLE_COUNT, this->verticalSonar) < 10) {
             this->display->clear();
-            this->display->write(0,"treasure detected");
-            graspSequence(i,height);
+            this->display->write(0, "treasure detected");
+            graspSequence(i, height);
             dropInBasket();
             break;
         }
     }
 }
 
-void Arm::graspSequence(double startingDist, double currentHeight){
-    //map distance away to additional distance required to travel (i.e. closer needs less distance)
-    //TO CHANGE, THIS IS CURRENLY CONSTANT:
-    double finalDist=startingDist+6;
-    sweep(startingDist,finalDist, currentHeight+1);
+void Arm::graspSequence(double startingDist, double currentHeight) {
+    // map distance away to additional distance required to travel (i.e. closer needs less distance)
+    // TO CHANGE, THIS IS CURRENLY CONSTANT:
+    double finalDist = startingDist + 6;
+    sweep(startingDist, finalDist, currentHeight + 1);
     display->clear();
-    display->write(0,"done final sweep");
-    moveInPlaneShoulderFirst(finalDist,currentHeight-3);
+    display->write(0, "done final sweep");
+    moveInPlaneShoulderFirst(finalDist, currentHeight - 3);
     display->clear();
-    display->write(0,"moved down");
+    display->write(0, "moved down");
     delay(1000);
     grasp();
     display->clear();
-    display->write(0,"done grasping");
+    display->write(0, "done grasping");
     delay(1000);
 }
 
 double Arm::getHypotenuse(double heightAboveGround, double distanceFromChassis) {
     double y = heightAboveGround - SHOULDER_HEIGHT;
-    double x = distanceFromChassis + SHOULDER_CHASSIS_EDGE_DIST; 
+    double x = distanceFromChassis + SHOULDER_CHASSIS_EDGE_DIST;
     return sqrt(pow(x, 2) + pow(y, 2));
 }
 
-
 double Arm::getPhi(double hypotenuse) {
-        double l1Squared = pow(BICEP_LENGTH, 2);
-        double l2Squared = pow(FOREARM_LENGTH, 2);
-        double l3Squared = pow(hypotenuse, 2);
-        double acosOut = acos((l1Squared + l2Squared - l3Squared) / (2 * FOREARM_LENGTH * BICEP_LENGTH));
-        return (RAD_TO_DEG * acosOut);
+    double l1Squared = pow(BICEP_LENGTH, 2);
+    double l2Squared = pow(FOREARM_LENGTH, 2);
+    double l3Squared = pow(hypotenuse, 2);
+    double acosOut = acos((l1Squared + l2Squared - l3Squared) / (2 * FOREARM_LENGTH * BICEP_LENGTH));
+    return (RAD_TO_DEG * acosOut);
 }
 
 double Arm::getTheta(double hypotenuse, double phi) {
@@ -160,61 +158,57 @@ double Arm::getTheta(double hypotenuse, double phi) {
 }
 
 double Arm::getAlpha(double heightAboveGround, double distanceFromChassis) {
-    double x = distanceFromChassis + SHOULDER_CHASSIS_EDGE_DIST; 
+    double x = distanceFromChassis + SHOULDER_CHASSIS_EDGE_DIST;
     double y = heightAboveGround - SHOULDER_HEIGHT;
-    return (RAD_TO_DEG * atan(y/x));
+    return (RAD_TO_DEG * atan(y / x));
 }
 
-double Arm::avgSampleSonar(int numReadings, NewPing* sonarSensor){
-    int sum=0;
+double Arm::avgSampleSonar(int numReadings, NewPing* sonarSensor) {
+    int sum = 0;
 
-    for (int i=0; i<numReadings; i++){
-        sum+=sonarSensor->ping_cm();
+    for (int i = 0; i < numReadings; i++) {
+        sum += sonarSensor->ping_cm();
     }
 
-    return ((double)sum/(double)numReadings);
-
+    return ((double)sum / (double)numReadings);
 }
 
-void Arm::testShoulder(){
-    for(int i=0;i<7;i++){
-        moveShoulderJoint(i*15);
+void Arm::testShoulder() {
+    for (int i = 0; i < 7; i++) {
+        moveShoulderJoint(i * 15);
         delay(2000);
     }
 }
 
-void Arm::testElbow(){
-    for(int i=0;i<10;i++){
-        this->elbow->write(i*15);
+void Arm::testElbow() {
+    for (int i = 0; i < 10; i++) {
+        this->elbow->write(i * 15);
         delay(2000);
     }
-
 }
 
-void Arm::testBase(){
-    for(int i=2;i<23;i++){
-        rotateBase(i*15);
+void Arm::testBase() {
+    for (int i = 2; i < 23; i++) {
+        rotateBase(i * 15);
         delay(2000);
     }
-
 }
 
-void Arm::testClaw(){
-    for(int i=3;i<10;i++){
-        this->claw->write(i*15);
+void Arm::testClaw() {
+    for (int i = 3; i < 10; i++) {
+        this->claw->write(i * 15);
         delay(1000);
     }
-
 }
 
-void Arm::testArm(){
+void Arm::testArm() {
     testShoulder();
     testElbow();
-    this->elbow->slowWrite(90,8);
+    this->elbow->slowWrite(90, 8);
     testBase();
     testClaw();
     rotateBase(180);
-    sweep(5,15,26);
+    sweep(5, 15, 26);
     this->claw->write(45);
     delay(500);
     this->claw->write(100);
--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,76 @@
 /**
  * @file      main.cpp
- * @author    Creators of CHONKY 
+ * @author    Creators of CHONKY
  * @brief     Main control loop for CHONKY codebase
  */
 
-#include "Arduino.h"
-#include <string.h>
 #include <NewPing.h>
-#include "servo.h"
-#include "PID.h"
-#include "motor.h"
-#include "pins.h"
+#include <string.h>
+
+#include "Arduino.h"
 #include "OLED.h"
+#include "PID.h"
 #include "arm.h"
 #include "constants.h"
+#include "motor.h"
+#include "pins.h"
+#include "servo.h"
 
 // Class instantiations
 // Motor rightMotor(RIGHT_MOTOR_A, RIGHT_MOTOR_B);
 // Motor leftMotor(LEFT_MOTOR_A, LEFT_MOTOR_B);
-Servo clawServo(CLAW_SERVO); //CLAW SERVO MUST BE INITIALIZED BEFORE SHOULDER DUE TO PIN DEPENDENCY
+Servo clawServo(CLAW_SERVO);  // CLAW SERVO MUST BE INITIALIZED BEFORE SHOULDER DUE TO PIN DEPENDENCY
 Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 OLED display(&display_handler);
-//PID tapeFollow(TapeFollower, &leftMotor, &rightMotor, &display);
+// PID tapeFollow(TapeFollower, &leftMotor, &rightMotor, &display);
 Servo elbowServo(ELBOW_SERVO);
-Servo baseServo (PA_8);
+Servo baseServo(PA_8);
 Motor shoulderMotor(PB_8, PB_9);
-NewPing horizontalSonar(RIGHT_TREASURE_TRIG, RIGHT_TREASURE_ECHO, 200); // NewPing setup of pins and maximum distance.
+NewPing horizontalSonar(RIGHT_TREASURE_TRIG, RIGHT_TREASURE_ECHO, 200);  // NewPing setup of pins and maximum distance.
 NewPing verticalSonar(LEFT_TREASURE_TRIG, LEFT_TREASURE_ECHO, 200);
-Arm mainArm(&shoulderMotor,&elbowServo,&clawServo, &baseServo, 150, &verticalSonar, &horizontalSonar, &display);
+Arm mainArm(&shoulderMotor, &elbowServo, &clawServo, &baseServo, 150, &verticalSonar, &horizontalSonar, &display);
 
 int potValue;
-int loopCount=0;                      
+int loopCount = 0;
 int treasureDistance;
-const int movingAvgRange=50;
+const int movingAvgRange = 50;
 int sonarDist[movingAvgRange];
 int sum;
-int threshold=59;
-int loopFlag=0;
-double loopAvg=0;
+int threshold = 59;
+int loopFlag = 0;
+double loopAvg = 0;
 
 void setup() {
     display.setUp();
     display.clear();
-    display.write(0,"yup");
-    pinMode(PA0,INPUT);
+    display.write(0, "yup");
+    pinMode(PA0, INPUT);
     delay(1000);
 }
 
-double ultrasonicAvg(int numReadings){
-    int sum=0;
+double ultrasonicAvg(int numReadings) {
+    int sum = 0;
     int reading;
 
-    for (int i=0; i<numReadings; i++){
-        reading=horizontalSonar.ping_cm();
-        if(reading==0){
-            sum+=400;
-        }
-        else{
-            sum+=reading;
+    for (int i = 0; i < numReadings; i++) {
+        reading = horizontalSonar.ping_cm();
+        if (reading == 0) {
+            sum += 400;
+        } else {
+            sum += reading;
         }
     }
-    return ((double)sum/(double)numReadings);
+    return ((double)sum / (double)numReadings);
 
-    pinMode(PA4,INPUT);
+    pinMode(PA4, INPUT);
 }
 
-void loop(){
-    //pwm_start(PB_8,100,1000,TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
-    //pwm_start(PB_9,100,1000,TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
-    //mainArm.moveShoulderJoint(90);    
-    //shoulderMotor.setSpeed(-100);
-    //mainArm.rotateBase(180);
+void loop() {
+    // pwm_start(PB_8,100,1000,TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
+    // pwm_start(PB_9,100,1000,TimerCompareFormat_t::RESOLUTION_12B_COMPARE_FORMAT);
+    // mainArm.moveShoulderJoint(90);
+    // shoulderMotor.setSpeed(-100);
+    // mainArm.rotateBase(180);
 
     // for(int i=0;i<6;i++){
     //     elbowServo.slowWrite(90,i*10);
@@ -78,7 +78,7 @@ void loop(){
     //     elbowServo.slowWrite(45,i*10);
     //     delay(3000);
     // }
-    
+
     // display.clear();
     // display.write(0,"done with alpha calc");
     // double shoulderJointAngle=alpha+theta;
@@ -91,10 +91,9 @@ void loop(){
     // delay(500);
     // mainArm.moveInPlaneShoulderFirst(3,26);
     // mainArm.moveInPlaneShoulderFirst(20,26);
-    //display.clear();
-    //display.write(0,std::to_string(analogRead(PA0)));
-    //mainArm.elbow->write(0);
-
+    // display.clear();
+    // display.write(0,std::to_string(analogRead(PA0)));
+    // mainArm.elbow->write(0);
 
     // display.clear();
     // display.write(0,std::to_string(analogRead(PA0)));
@@ -103,7 +102,6 @@ void loop(){
     // display.write(0,"treasure detected");
     // //mainArm.testShoulder();
 
-    
     // display.clear();
     // display.write(0,std::to_string((int)mainArm.avgSampleSonar(40)));
     // mainArm.rotateBase(90);
@@ -121,10 +119,10 @@ void loop(){
     // delay(1000);
     // mainArm.sweepAndDetect(3,16,26);
 
-    //display.clear();
-    //display.write(0,std::to_string(analogRead(PA0)));
+    // display.clear();
+    // display.write(0,std::to_string(analogRead(PA0)));
 
-    //mainArm.testShoulder();
+    // mainArm.testShoulder();
 
     // for(double i=3;i<16;i+=SWEEP_STEP_SIZE){
     //         loopAvg=0;
@@ -144,41 +142,39 @@ void loop(){
     //             display.write(0,"treasure detected");
     //             break;
     //         }
-    //    }  
+    //    }
 
-    //WORKING FOR TIME TRIALS
-    // mainArm.moveShoulderJoint(90);
-    // mainArm.elbow->write(90);
-    // delay(3000);
-    // mainArm.sweepAndDetect(3,20,35);
-    // delay(3000);
+    // WORKING FOR TIME TRIALS
+    //  mainArm.moveShoulderJoint(90);
+    //  mainArm.elbow->write(90);
+    //  delay(3000);
+    //  mainArm.sweepAndDetect(3,20,35);
+    //  delay(3000);
 
-    //WORKING TREASURE GRASPING CODE
+    // WORKING TREASURE GRASPING CODE
     mainArm.rotateBase(270);
     mainArm.moveShoulderJoint(90);
-    mainArm.moveInPlaneElbowFirst(6,25);
+    mainArm.moveInPlaneElbowFirst(6, 25);
     delay(2000);
-    for (int i=220;i<300; i++){
+    for (int i = 220; i < 300; i++) {
         mainArm.rotateBase(i);
-        int ultraSonicReading=ultrasonicAvg(50);
-        if(ultraSonicReading<25){
+        int ultraSonicReading = ultrasonicAvg(50);
+        if (ultraSonicReading < 25) {
             display.clear();
-            display.write(0,"treasure detected");
-            int secondaryVerification=ultrasonicAvg(50);
-            if(secondaryVerification<25){
-                display.write(20,"secondary verification passed");
-                mainArm.rotateBase(i+4);
-                mainArm.sweepAndDetect(5,20,35);
+            display.write(0, "treasure detected");
+            int secondaryVerification = ultrasonicAvg(50);
+            if (secondaryVerification < 25) {
+                display.write(20, "secondary verification passed");
+                mainArm.rotateBase(i + 4);
+                mainArm.sweepAndDetect(5, 20, 35);
                 break;
             }
         }
     }
 
     // display.clear();
-    ///ROTATION DETECTION
-    //if (loopFlag==0) {
-
-
+    /// ROTATION DETECTION
+    // if (loopFlag==0) {
 
     // display.clear();
     // display.write(20,std::to_string((int)loopAvg));
@@ -193,16 +189,12 @@ void loop(){
     // int before = millis();
 
     // ultrasonicAvg(100);
-    
+
     // int after=millis();
 
     // display.clear();
     // display.write(0, std::to_string((int(after-before))));
     // delay(1000);
-
-   
-
-
 
     // loopCount++;
     // if (loopCount>movingAvgRange){
@@ -217,7 +209,6 @@ void loop(){
     //         sum+=sonarDist[i];
     //     }
 
-        
     //     display.clear();
 
     //     if ((sum/movingAvgRange)<threshold){
@@ -236,5 +227,4 @@ void loop(){
     //     display.clear();
     //     display.write(0,std::to_string(loopCount));
     // }
-
 }
